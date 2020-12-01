7a32e74aa6409c5e51b1b336873a41d7bf12bd44
Mirelle Nascimento <mmirellenascimento@gmail.com>
2020-12-01 17:19:43 +0100

    Part - 02 (Bonus)

--- js/main.js
+++ js/main.js
@@ -2,17 +2,28 @@ const mySpotify = new Vue({
   el: '#root',
   data: {
     albums: [],
-    filteredAlbuns: [],
+    filteredAlbums: [],
+    selectedGenre: 'all',
+  },
+  methods:{
 
+    //Filters albums by genre at user's input
+    selectGenre(event){
+      this.selectedGenre = event.target.value;
+      if (this.selectedGenre === 'all'){
+        this.filteredAlbums = [...this.albums];
+      } else {
+        this.filteredAlbums = this.albums.filter(album => album.genre.toLowerCase() === this.selectedGenre.toLowerCase());
+      }
+    },
   },
   mounted(){
+    //Imports data to albums ans filteredAlbums arrays
     axios
     .get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(gotAlbums => {
       this.albums = gotAlbums.data.response;
-      console.log(gotAlbums.data.response);
-      console.log(this.albums);
+      this.filteredAlbums = [...this.albums];
     });
-  }
-
+  },
 });
